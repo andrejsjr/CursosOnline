a26c943454b9c14909950d1f47406c212b324215
andrejsjr <andrejsjr@gmail.com>
2018-08-09 17:55:27 -0300

    Alura Type

--- Alura/FrontEnd/JavaScript/jquery/alura-typer/public/js/main.js
+++ Alura/FrontEnd/JavaScript/jquery/alura-typer/public/js/main.js
@@ -1,39 +1,71 @@
-var tamanhoFrase = $(".frase").text().split(" ").length;
-$("#tamanho-frase").text(tamanhoFrase);
-
 var tempoDigitacaoInicial = $("#tempo-digitacao").text();
 
-var campoDigitacao = $(".campo-digitacao");
-var contadorCaracteres = $("#contador-caracteres");
-var contadorPalavras = $("#contador-palavras");
-
-campoDigitacao.on("input", function () {
-    contadorCaracteres.text(campoDigitacao.val().length);
-    contadorPalavras.text(campoDigitacao.val().split(/\S+/).length - 1);
+$(function () {
+    atualizaTamanhoFrase();
+    atribuiEventos();
 });
 
-var disparaCronometro = function () {
+var disparaCronometro = function () {    
+    $("#botao-reiniciar").attr("disabled", true);
+    
     var tempoDigitacao = tempoDigitacaoInicial;
     
     var cronometroID = setInterval(function () {
         $("#tempo-digitacao").text(--tempoDigitacao);
         if (!tempoDigitacao) {
-            campoDigitacao.attr("disabled", true);
+            $(".campo-digitacao").attr("disabled", true);
+            $(".campo-digitacao").toggleClass("campo-desabilitado");
+            $("#botao-reiniciar").attr("disabled", false);
             clearInterval(cronometroID);
         }
     }, 1000);
 };
 
-campoDigitacao.one("focus", disparaCronometro);
-
-$("#botao-reiniciar").click(function () {
+var reiniciaJogo = function () {
     $("#tempo-digitacao").text(tempoDigitacaoInicial);
     
-    campoDigitacao.attr("disabled", false);
-    campoDigitacao.val("");
-
-    contadorCaracteres.text(0);
-    contadorPalavras.text(0);
-
+    $(".campo-digitacao").attr("disabled", false);
+    $(".campo-digitacao").val("");
+    $(".campo-digitacao").toggleClass("campo-desabilitado");
+    $(".campo-digitacao").removeClass("campo-correto");
+    $(".campo-digitacao").removeClass("campo-errado");
+    
+    $("#contador-caracteres").text(0);
+    $("#contador-palavras").text(0);
+    
     disparaCronometro();
-});
+};
+
+function atualizaBorda() {
+    var comparavel = $(".frase").text().substr(0, $(".campo-digitacao").val().length);
+
+    if ($(".campo-digitacao").val()) {
+        if ($(".campo-digitacao").val() == comparavel) {
+            $(".campo-digitacao").addClass("campo-correto");
+            $(".campo-digitacao").removeClass("campo-errado");
+        } else {
+            $(".campo-digitacao").addClass("campo-errado");            
+            $(".campo-digitacao").removeClass("campo-certo");
+        }
+    } else {
+        $(".campo-digitacao").removeClass("campo-correto");
+        $(".campo-digitacao").removeClass("campo-errado");
+    }
+};
+
+function atribuiEventos() {
+    $(".campo-digitacao").on("input", function () {
+        $("#contador-caracteres").text($(".campo-digitacao").val().length);
+        $("#contador-palavras").text($(".campo-digitacao").val().split(/\S+/).length - 1);
+        atualizaBorda();
+    });
+    
+    $(".campo-digitacao").one("focus", disparaCronometro);
+    
+    $("#botao-reiniciar").click(reiniciaJogo);
+};
+
+function atualizaTamanhoFrase() {
+    var tamanhoFrase = $(".frase").text().split(" ").length;
+    $("#tamanho-frase").text(tamanhoFrase);
+};
