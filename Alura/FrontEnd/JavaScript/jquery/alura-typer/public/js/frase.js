da3ac2f9eab1813139627dbc05de1e2517abc5bb
andrejsjr <andrejsjr@gmail.com>
2018-09-11 01:24:09 -0300

    Alura Typer Mod 2 - frase

--- Alura/FrontEnd/JavaScript/jquery/alura-typer/public/js/frase.js
+++ Alura/FrontEnd/JavaScript/jquery/alura-typer/public/js/frase.js
@@ -1,3 +1,8 @@
 function fraseAleatoria() {
-// Teste commimt notebook
+    $.get("http://localhost:3000/frases", alteraFraseAleatoria);
+}
+
+function alteraFraseAleatoria(data) {
+    var numAleatorio = Math.floor(Math.random() * data.length);
+    $(".frase").text(data[numAleatorio].texto);
 }
