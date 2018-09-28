9385b233a0d84960eeb26d17e33963938655fe73
andrejsjr <andrejsjr@gmail.com>
2018-09-28 17:55:54 -0300

    Alura Frame - Updates

--- Alura/FrontEnd/JavaScript/javascript-avancado-i/aluraframe/client/js/app/controllers/NegociacaoController.js
+++ Alura/FrontEnd/JavaScript/javascript-avancado-i/aluraframe/client/js/app/controllers/NegociacaoController.js
@@ -7,35 +7,53 @@ class NegociacaoController {
         this._inputQuantidade = $('#quantidade');
         this._inputValor = $('#valor');
 
-        this._listaNegociacoes = new ListaNegociacoes(model => 
-            this._negociacoesView.update(model));
+        this._listaNegociacoes = new Bind(new ListaNegociacoes(), 
+            new NegociacoesView($('#negociacoes-view')), 
+            'adiciona', 'esvazia');
 
-        this._negociacoesView = new NegociacoesView($('#negociacoes-view'));
-        this._negociacoesView.update(this._listaNegociacoes);
-
-        this._mensagem = new Mensagem();
-        
-        this._mensagemView = new MensagemView($('#mensagem-view'));
-        this._mensagemView.update(this._mensagem);
+        this._mensagem = new Bind(new Mensagem(), 
+            new MensagemView($('#mensagem-view')), 'texto');
     }
     
     adiciona(event) {
         event.preventDefault();
 
-        this._listaNegociacoes.adiciona(this._criaNegociacao());
-        
-        this._mensagem.texto = 'Negociação incluida com sucesso';
-        this._mensagemView.update(this._mensagem);
-        
+        this._listaNegociacoes.adiciona(this._criaNegociacao());        
+        this._mensagem.texto = 'Negociação incluida com sucesso';        
         this._limpaFormulario();
     }
+    
+    importaNegociacoes() {
+        let xhr = new XMLHttpRequest();
+        
+        xhr.open('GET', 'nogociacoes/semana');
 
+        xhr.onreadystatechange = () => {
+            /*
+                0: requisição ainda não iniciada
+                1: conexão com o servidor estabelecida
+                2: requisição recebida
+                3: processando requisicao
+                4: requisição concluída e a resposta está pronta
+            */
+
+            if (xhr.readyState == 4) {
+                if (xhr.status = 200) {
+
+                }
+            }
+        };
+
+        xhr.send();
+    }
+    
     esvazia() {
         this._listaNegociacoes.esvazia();
         
         this._mensagem.texto = 'Negociações apagadas com sucesso';
         this._mensagemView.update(this._mensagem);
     }
+
 
     _criaNegociacao() {
         return new Negociacao(
