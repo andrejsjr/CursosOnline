f156afe6138d811eeb87a39a1429bf24d5669172
andrejsjr <andrejsjr@gmail.com>
2019-03-29 17:31:32 -0300

    Curso Node

--- Alura/BackEnd/Node.js/CASADOCODIGO/src/app/rotas/rotas.js
+++ Alura/BackEnd/Node.js/CASADOCODIGO/src/app/rotas/rotas.js
@@ -11,21 +11,24 @@ module.exports = app => {
                     </body>
                 </html>
             `
-        );
+        );        
     });
     
     app.get('/livros', (req, resp) => {
-        resp.end(
-            `
-                <html>
-                    <head>
-                        <meta charset="utf-8">
-                    </head>
-                    <body>
-                        <h1> Listagem de Livros </h1>
-                    </body>
-                </html>
-            `
+        resp.marko(
+            require('../views/livros/lista/lista.marko'),
+            {
+                livros: [
+                    {
+                        id: 1,
+                        titulo: 'Fundamentos do Node'
+                    },
+                    {
+                        id: 2,
+                        titulo: 'Node Avançado'
+                    }
+                ]
+            }
         );
     });
 };
