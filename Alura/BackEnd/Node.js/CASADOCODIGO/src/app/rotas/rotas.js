885f93e1fbbd5527496f1099b705ac62e2ec47dd
andrejsjr <andrejsjr@gmail.com>
2019-04-04 17:50:36 -0300

    Curso Node

--- Alura/BackEnd/Node.js/CASADOCODIGO/src/app/rotas/rotas.js
+++ Alura/BackEnd/Node.js/CASADOCODIGO/src/app/rotas/rotas.js
@@ -30,4 +30,24 @@ module.exports = app => {
             })
             .catch(erro => console.log(erro));
     });
+
+    app.get('/livros/form', (req, resp) => {
+        resp.marko(require('../views/livros/form/form.marko'));
+    });
+
+    app.post('/livros', (req, resp) => {
+        const livroDao = new LivroDao(db);
+        livroDao.adiciona(req.body)
+            .then(resp.redirect('/livros'))
+            .catch(erro => console.log(erro));
+    });
+
+    app.delete('/livros/:id', (req, resp) => {
+        const id = req.params.id;
+
+        const livroDao = new LivroDao(db);
+        livroDao.remove(id)
+            .then(() => resp.status(200).end())
+            .catch(erro => console.log(erro));
+    });
 };
