3e964f48b239122104561e0b29655b0e22696f7b
andrejsjr <andrejsjr@gmail.com>
2019-04-09 15:55:50 -0300

    JS Masterclass

--- RodrigoBranas/JavaScript_Masterclass/main.js
+++ RodrigoBranas/JavaScript_Masterclass/main.js
@@ -1,23 +1,35 @@
-const statement = 'create table author (id number, name string, age number, city string, state string, country string)';
-const regexp = /create table (?<tabela>[a-z]+) \((?<colunas>.+)\)/;
-const parsedStatement = statement.match(regexp);
-const tableName = parsedStatement[1];
-let columns = parsedStatement[2];
-columns = columns.split(', ');
 const database = {
     tables: {
-        [tableName]: {
+        
+    },
+    
+    createTable(statement) {
+        const regexp = /create table (?<tabela>[a-z]+) \((?<colunas>.+)\)/;
+        const parsedStatement = statement.match(regexp);
+        const tableName = parsedStatement[1];
+        
+        this.tables[tableName] = {
             columns: {},
             data: []
+        };
+        
+        let columns = parsedStatement[2];
+        columns = columns.split(', ');        
+        
+        for (let column of columns) {
+            column = column.split(' ');
+            const name = column[0];
+            const type = column[1];
+            this.tables[tableName].columns[name] = type;           
         }
+    },
+
+    execute(statement) {
+        if (statement.startsWith('create table'))
+            return this.createTable(statement);
     }
 };
 
-for (let column of columns) {
-    column = column.split(' ');
-    const name = column[0];
-    const type = column[1];
-    database.tables[tableName].columns[name] = type;           
-}
+database.execute('create table author (id number, name string, age number, city string, state string, country string)');
 
 console.log(JSON.stringify(database, undefined, '  '));
