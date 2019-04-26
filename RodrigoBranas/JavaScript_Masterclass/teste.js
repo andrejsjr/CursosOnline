e498a04979068236effd7cfa5307091c79c54b3b
unknown <desenvolvedor14@NEXXUS.LOCAL>
2019-04-26 17:57:50 -0300

    Javascript Masterclass

--- RodrigoBranas/JavaScript_Masterclass/teste.js
+++ RodrigoBranas/JavaScript_Masterclass/teste.js
@@ -1,16 +1,2 @@
-const puta1 = {
-    nome: 'Camila',
-    idade: 20,
-    oral: true,
-    vaginal: true,
-    anal: false
-}
-
-function getPuta(...putas) {
-    console.log(putas);
-    for (let puta of putas) {
-        console.log(puta);
-    }
-}
-
-getPuta(puta1);
+const putas = new Set(['Patricia', 'Patricia']);
+console.log(putas);
