f1a7c7e96d90d7dd86835fcfb7b0444beb7bcf2f
jesus <andre.jesus@nexxus-sistemas.com.br>
2019-10-11 18:00:23 -0300

    AluraPic Updates

--- Alura/FrontEnd/MVC/Angular/alurapic/src/app/photos/photo-list/photo-list.component.ts
+++ Alura/FrontEnd/MVC/Angular/alurapic/src/app/photos/photo-list/photo-list.component.ts
@@ -1,7 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 
-import { PhotoService } from '../photo/photo.service';
 import { Photo } from '../photo/photo';
 
 @Component({
@@ -10,17 +9,16 @@ import { Photo } from '../photo/photo';
   styleUrls: ['./photo-list.component.css']
 })
 export class PhotoListComponent implements OnInit {
-  photos: Photo[] = [];
   
-  constructor(
-    private photoService: PhotoService,
-    private activatedRoute: ActivatedRoute
-  ) { }
+  photos: Photo[] = [];
+  filter: string = '';
+  
+  constructor(private activatedRoute: ActivatedRoute) { }
   
   ngOnInit(): void {
-    const userName = this.activatedRoute.snapshot.params.userName;
-    this.photoService
-      .listFromUser(userName)
-      .subscribe(photos => this.photos = photos);
+    // this.activatedRoute.snapshot.data
+    // dá acesso ao objeto que está em resolve
+    // na configuração da rota
+    this.photos = this.activatedRoute.snapshot.data['photos'];
   }
 }
