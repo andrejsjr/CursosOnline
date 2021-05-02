5659a04226fe23a1c2f8dc7a2ef8238abe71f28d
jesus <andre.jesus@nexxus-sistemas.com.br>
2021-05-02 01:35:44 -0300

    alurapic Update

--- Alura/FrontEnd/MVC/Angular/alurapic/src/app/app.module.ts
+++ Alura/FrontEnd/MVC/Angular/alurapic/src/app/app.module.ts
@@ -2,13 +2,21 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { PhotosModule } from './photos/photos.module';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    /*
+      BrowserModule contem recursos do Angular
+      para serem usados no browser.
+      Por isso, malandramente, o Angular já o importa
+      automaticamente.
+    */
+    BrowserModule,
+    PhotosModule
   ],
   providers: [],
   bootstrap: [AppComponent]
