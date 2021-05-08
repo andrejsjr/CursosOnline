269f757b8a0e3572ef27ea81055953c18f392fdd
jesus <andre.jesus@nexxus-sistemas.com.br>
2021-05-08 11:55:50 -0300

    alurapic updates

--- Alura/FrontEnd/MVC/Angular/alurapic/src/app/photos/photo-list/photo-list.module.ts
+++ Alura/FrontEnd/MVC/Angular/alurapic/src/app/photos/photo-list/photo-list.module.ts
@@ -8,6 +8,7 @@ import { CommonModule } from '@angular/common';
 import { PhotoModule } from '../photo/photo.module';
 import { CardModule } from 'src/app/shared/components/card/card.module';
 import { SearchComponent } from './search/search.component';
+import { DarkenOnHoverModule } from 'src/app/shared/directives/darken-on-hover/darken-on-hover.module';
 
 @NgModule({
     declarations: [
@@ -28,7 +29,8 @@ import { SearchComponent } from './search/search.component';
         */
         CommonModule,
         PhotoModule,
-        CardModule
+        CardModule,
+        DarkenOnHoverModule
     ]
 })
 export class PhotoListModule { }
---
+++ Alura/FrontEnd/MVC/Angular/alurapic/src/app/shared/directives/darken-on-hover/darken-on-hover.directive.ts
@@ -0,0 +1,72 @@
+import { Directive, ElementRef, HostListener, Input, Renderer } from '@angular/core';
+import { renderComponent } from '@angular/core/src/render3';
+
+/*
+    Diretivas são instruções (códigos de programação)
+    aplicadas a um template.
+
+    Diretivas são aplicadas diretamente ao elemento
+    do DOM como se fosse um atributo. Ex:
+
+    <a apDarkOnHover></a>
+
+    Componentes também são diretivas,
+    porém são diretivas que possuem um template
+    associado.
+*/
+@Directive({
+    selector: '[apDarkenOnHover]'
+})
+export class DarkenOnHoverDirective {
+
+    @Input() brightness = '70%';
+    
+    /*
+        O Angular injetará em el, uma referência para
+        o elemento do DOM onde a diretiva foi aplicada,
+        permitindo assim a sua manipulação no TypeScript.
+        ElementRef é uma classe do Angular, uma camada
+        de proteção sobre o elemento nativo.
+    */
+    constructor(
+        private el: ElementRef,
+        private render: Renderer) { }
+
+    /*
+        @HostListener marca o método para responder
+        ao evento JavaScript passado no parâmetro.
+    */
+    @HostListener('mouseover') // Quando passar o mouse em cima.
+    darkenOn() {
+        /*
+            Não manipularemos o DOM diretamente através de el.
+            Usaremos o Renderer porque ele garante a rendereção
+            do DOM mesmo no backend (Angular Universe).
+        */
+
+        /*
+            Aplicando estilo em el.
+            Propriedade filter, valor brightness(70%)
+        */
+        this.render.setElementStyle(this.el.nativeElement, 'filter', `brightness(${this.brightness})`);
+    }
+
+    /*
+        @HostListener marca o método para responder
+        ao evento JavaScript passado no parâmetro.
+    */
+    @HostListener('mouseleave') // Quando tirar o mouse de cima.
+    darkenOff() {
+        /*
+            Não manipularemos o DOM diretamente através de el.
+            Usaremos o Renderer porque ele garante a rendereção
+            do DOM mesmo no backend (Angular Universe).
+        */
+
+        /*
+            Aplicando estilo em el.
+            Propriedade filter, valor brightness(70%)
+        */
+        this.render.setElementStyle(this.el.nativeElement, 'filter', 'brightness(100%)');
+    }
+}
